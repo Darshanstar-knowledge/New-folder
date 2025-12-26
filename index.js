5da4d199171046ea91a8eb23efc1dd8d6965c9d2
Darshan <Darshan.pk@star-knowledge.org>
2025-12-26 20:08:48 +0530

    new

---
+++ index.js
@@ -0,0 +1,16 @@
+// $('.cards-carousel').slick();
+
+
+
+
+$('.cards-carousel').slick({
+    arrows:false,
+    infinite: true,
+    autoplay:true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+   
+    dots:true,
+  });
